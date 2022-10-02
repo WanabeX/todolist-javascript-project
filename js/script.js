733dc6b2626670b9745b27cb238e0dd6b4ba15e6
WanabeX <wanaberecoder@gmail.com>
2022-10-03 02:34:30 +0900

    Feat: task 삭제기능 추가 Chore: 주석을 이용하여 HTML 문서 내 파트 구분

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function showTask() {
             </label>
               <div class="settings">
                 <ion-icon name="create-outline"></ion-icon>
-                <ion-icon name="trash"></ion-icon>
+                <ion-icon onclick="removeTask(${id})"name="trash"></ion-icon>
               </div>`;
     });
   }
@@ -28,19 +28,24 @@ function showTask() {
 }
 showTask();
 
-// check the task status and updating
+// checking status and add checked CSS
 function toggleCheckedStyle(checkedTask) {
-  // Get the task name in <span>tag
+  // Get the task name from <span>tag
   const taskName = checkedTask.parentElement.lastElementChild;
   // If task "checked"
   if (checkedTask.checked) {
-    // Add the css line style at task
     taskName.classList.add("checked");
   } else {
-    // Remove the css line style at task
     taskName.classList.remove("checked");
   }
 }
+
+// remove task
+removeTask = (removeId) => {
+  tasks.splice(removeId, 1);
+  localStorage.setItem(TASK_KEY, JSON.stringify(tasks));
+  showTask();
+};
 
 // if input field is not empty, add button is activated
 taskInput.onkeyup = () => {
@@ -71,5 +76,6 @@ taskInput.addEventListener("keyup", (e) => {
   let userTask = taskInput.value.trim();
   if (e.key == "Enter" && userTask) {
     addBtn.click();
+    //addBtn.classList.remove("active");
   }
 });
